d49f35cae8f229069c6330d388cc6c2c255fe23a
unknown <damydavo2015@gmail.com>
2022-03-18 21:48:27 +0100

    minified css and javascript

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-let themeDots = document.getElementsByClassName("theme-dot");
-for (let i = 0; themeDots.length > i; i++) {
-  themeDots[i].addEventListener("click", function () {
-    let mode = this.dataset.mode;
-    setTheme(mode);
-  });
-}
-
-function setTheme(mode) {
-  if (mode == "light") {
-    document.getElementById("theme-style").href = "css/style.css";
-  }
-
-  if (mode == "blue") {
-    document.getElementById("theme-style").href = "css/blue.css";
-  }
-
-  if (mode == "green") {
-    document.getElementById("theme-style").href = "css/green.css";
-  }
-
-  if (mode == "purple") {
-    document.getElementById("theme-style").href = "css/purple.css";
-  }
-}
+let themeDots=document.getElementsByClassName("theme-dot");for(let i=0;themeDots.length>i;i++){themeDots[i].addEventListener("click",function(){let mode=this.dataset.mode;setTheme(mode)})}
+function setTheme(mode){if(mode=="light"){document.getElementById("theme-style").href="css/style.css"}
+if(mode=="blue"){document.getElementById("theme-style").href="css/blue.css"}}
