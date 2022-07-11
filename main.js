c0effb46ee638b129025e5537df99dd658c7ee17
unknown <damydavo2015@gmail.com>
2022-07-11 23:02:58 +0100

    make the navbar sticky

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 let themeDots=document.getElementsByClassName("theme-dot");for(let i=0;themeDots.length>i;i++){themeDots[i].addEventListener("click",function(){let mode=this.dataset.mode;setTheme(mode)})}
 function setTheme(mode){if(mode=="light"){document.getElementById("theme-style").href="css/style.css"}
 if(mode=="blue"){document.getElementById("theme-style").href="css/blue.css"}}
+
+
+window.onscroll = () => getWrapper()
+
+const navBar = document.querySelector('.nav-wrapper')
+const sticky = navBar.offsetTop
+
+
+const getWrapper = () => window.pageYOffset > sticky ? navBar.classList.add('sticky') : navBar.classList.remove('sticky')
+
+
+
+
